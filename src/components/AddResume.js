220856cc37c1616c115a0a421b80c12f13adab6d
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-01-12 10:28:05 -0500

    added react-dropzone-uploader

--- src/components/AddResume.js
+++ src/components/AddResume.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Dropzone from 'react-dropzone';
+import Dropzone from 'react-dropzone-uploader';
+import 'react-dropzone-uploader/dist/styles.css';
 import {
   Button,
   ModalContent,
@@ -17,8 +18,33 @@ import {
   Textarea,
 } from '@chakra-ui/react';
 
+const Preview = ({ meta }) => {
+  const { name, percent, status } = meta;
+  return (
+    <span
+      style={{
+        alignSelf: 'flex-start',
+        margin: '10px 3%',
+        fontFamily: 'Helvetica',
+      }}
+    >
+      {name}, {Math.round(percent)}%, {status}
+    </span>
+  );
+};
+
 export default function AddResume() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const getUploadParams = ({ meta }) => {
+    return { url: 'https://httpbin.org/post' };
+  };
+
+  const handleSubmit = (files, allFiles) => {
+    console.log(files.map((f) => f.meta));
+    allFiles.forEach((f) => f.remove());
+  };
+
   return (
     <>
       <Button onClick={onOpen}>Add Resume</Button>
@@ -28,13 +54,26 @@ export default function AddResume() {
         <ModalContent>
           <ModalHeader>Add Resume</ModalHeader>
           <ModalCloseButton />
-          <FormControl id='resume-nickname' isRequired>
-            <Input placeholder='Resume Nickname' />
+          <FormControl id="resume-nickname" isRequired>
+            <Input placeholder="Resume Nickname" />
           </FormControl>
-
-          <ModalFooter>
-            <Button colorScheme='blue'>Submit</Button>
-          </ModalFooter>
+          <Dropzone
+            getUploadParams={getUploadParams}
+            onSubmit={handleSubmit}
+            PreviewComponent={Preview}
+            inputContent="Drop Files"
+            disabled={(files) =>
+              files.some((f) =>
+                ['preparing', 'getting_upload_params', 'uploading'].includes(
+                  f.meta.status
+                )
+              )
+            }
+            SubmitButtonComponent={() => (
+              <Button colorScheme="blue">Submit</Button>
+            )}
+          />
+          <ModalFooter></ModalFooter>
         </ModalContent>
       </Modal>
     </>
