f9658095eb4e6d49519804b9596aa6fac341f692
Lourent Flores <lourent.flores@gmail.com>
2021-01-12 16:58:34 -0500

    updated dashController with post

--- server/controllers/dashController.js
+++ server/controllers/dashController.js
@@ -3,11 +3,12 @@ const db = require('../models/userModel');
 const dashController = {};
 
 dashController.getResumes = async (req, res, next) => {
-  const { id } = req.params;
+  const { id } = req.params
   const resumeQuery = `SELECT res_name FROM resume_table WHERE user_id = $1`;
   try {
     const data = await db.query(resumeQuery, [id]);
-    res.locals.resume = data.rows[0]
+    res.locals.resume = data.rows;
+    console.log(res.locals.resume)
     return next();
   } catch (err) {
     next({
@@ -34,35 +35,48 @@ dashController.getApplications = async (req, res, next) => {
 
 dashController.postResume = async (req, res, next) => {
   const { resName, link } = req.body;
-  const { id } = req.params
-  const postResumeQuery = `INSERT INTO resume_table (res_name, link, user_id) VALUES ($1, $2, $3)`
-  const queryParams = [resName, link, id]
+  const { id } = req.params;
+  const postResumeQuery = `INSERT INTO resume_table (res_name, link, user_id) VALUES ($1, $2, $3)`;
+  const queryParams = [resName, link, id];
+
   try {
-    const data = await db.query(postResumeQuery, queryParams);
-    res.locals.postResume = data.rows[0];
+    await db.query(postResumeQuery, queryParams);
     return next();
   } catch (err) {
     next({
       log: `dashController.postResume: ERROR: ${err}`,
-      message: { err: 'Error occurred in postResume controller' }
+      message: { err: 'Error occurred in postResume controller' },
     });
   }
-}
+};
 
 dashController.postApplication = async (req, res, next) => {
   const { id } = req.params;
-  const { companyName, rating, jobPostLink, status, comments, techStack } = req.body;
-  const getResumeId = `SELECT id FROM resume_table WHERE user_id = $1`
- 
-  try {
-    const resumeId = await db.query(getResumeId, [id]);
-    const queryParams = [companyName, rating, jobPostLink, status, comments, techStack, resumeId, id]
+  const {
+    companyName,
+    rating,
+    jobPostLink,
+    status,
+    comments,
+    techStack,
+    resumeId,
+  } = req.body;
+  
+  const postApplicationQuery = `INSERT INTO application_table (company_name, rating, job_post_link, status, comments, tech_stack, resume_id, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
 
-    const postApplicationQuery = `INSERT INTO application_table (company_name, rating, job_post_link, status, comments, tech_stack, resume_id, user_id) VALUES ($2, $3, $4, $5, $6,$7, ${resumeId}, $1)`;
-    
-    const data = await db.query(postApplicationQuery, queryParams)
-    console.log(data)
-    res.locals.postApplication = data.rows[0];
+  const queryParams = [
+    companyName,
+    rating,
+    jobPostLink,
+    status,
+    comments,
+    techStack,
+    resumeId,
+    id,
+  ];
+
+  try {
+    await db.query(postApplicationQuery, queryParams);
     return next();
   } catch (err) {
     next({
@@ -70,6 +84,6 @@ dashController.postApplication = async (req, res, next) => {
       message: { err: 'Error occurred in postApplication controller' },
     });
   }
-}
+};
 
 module.exports = dashController;
