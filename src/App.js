de885097591ada195dd5dc4d7c4f58a3d3d7d50b
Kyle Juresich <kjuresich@gmail.com>
2021-01-12 16:16:06 -0500

    built most of the logic for login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import AddApplication from './components/AddApplication';
 import AddResume from './components/AddResume';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 const App = () => {
   return (
     <div>
+      <Login />
       <AddApplication />
       <AddResume />
+
+      <Signup />
     </div>
   );
 };
