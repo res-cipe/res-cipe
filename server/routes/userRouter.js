33d35e26b7f585c874779c12924875e5fc145a2a
Jennifer Courtner <jmichele.courtner@gmail.com>
2021-01-12 19:38:02 -0500

    package.json updated, userRouter, userController, server

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const userController = require('../controllers/userController');
 
 router.post('/', userController.newUser, (req, res) => {
-  res.status(200).json([res.locals]);
+  // res.status(200).json([res.locals]);
+  console.log('this is req.user', req.user);
+  console.log('this is req.Authenticated', req.isAuthenticated());
+  res.redirect('/');
 });
 
 // router.get();
