75d6b7594a2297e640292d68a753624c9ddb40f4
Sabrina <s.goldfarb2@gmail.com>
2021-01-13 15:23:13 -0600

    implemented bcrypt, passport, auth

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -8,9 +8,17 @@ router.post('/', userController.newUser, (req, res) => {
   // res.status(200).json([res.locals]);
   console.log('this is req.user', req.user);
   console.log('this is req.Authenticated', req.isAuthenticated());
-  res.redirect('/');
+  if (req.isAuthenticated()) {
+    res.locals.isLoggedIn = true;
+    console.log(res.locals);
+    res.status(200).json(res.locals);
+
+  } else {  
+    res.locals.isLoggedIn = false;
+    res.status(400).json(res.locals.isLoggedIn);
+  }
+
 });
 
-// router.get();
 
 module.exports = router;
