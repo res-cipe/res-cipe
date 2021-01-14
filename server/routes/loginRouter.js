ccfd408c45874d8b684f13e10de64a48dff6d3bd
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-01-14 01:12:04 -0500

    changed login route to return the userId in the response after succesfully logging in and creating a session

--- server/routes/loginRouter.js
+++ server/routes/loginRouter.js
@@ -1,15 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
 
-const userController = require('../controllers/userController');
-
-
-router.post('/', passport.authenticate('local', {
-	successRedirect: '/',
-	failureRedirect: '/'
-}));
-
+router.post('/', passport.authenticate('local'), (req, res) => {
+  res.status(200).json(req.user);
+});
 
 module.exports = router;
