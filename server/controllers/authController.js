1f1d4d2cc2a10ca329c757fae15842965afb6314
jasonligg <9956289+jasonligg@users.noreply.github.com>
2021-01-14 01:14:12 -0500

    conditional now correctly grabs the userId off the req.session object and now properly verifies session

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,24 +1,24 @@
 const db = require('../models/userModel');
 const session = require('express-session');
 
-const authController ={};
-
+const authController = {};
 
 authController.verifyUser = (req, res, next) => {
-
   try {
-    if(req.session.passport.user && req.session.passport.user == req.params.id) {
-      next();
+    if (
+      req.session.passport.user.userId &&
+      req.session.passport.user.userId == req.params.id
+    ) {
+      return next();
     } else {
-      res.sendStatus(401)
+      res.sendStatus(401);
     }
   } catch (error) {
-    next({
+    return next({
       log: `authController.verifyUser: ERROR: ${error}`,
       message: { error: 'An internal error occured :( ' },
-    })
+    });
   }
-
 };
 
 module.exports = authController;
