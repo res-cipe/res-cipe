c0670654c0bc7c1f48a8692067f764732e50d971
Jennifer Courtner <jmichele.courtner@gmail.com>
2021-02-04 10:29:09 -0500

    took connection string out of userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
-const PG_URI =
-  'postgres://yfopfigc:NdRchGgXUa0D2bkRE4haivaL7eXpn86w@ziggy.db.elephantsql.com:5432/yfopfigc';
+// enter connection string for your database
+const PG_URI = '';
 
 const pool = new Pool({
   connectionString: PG_URI,
